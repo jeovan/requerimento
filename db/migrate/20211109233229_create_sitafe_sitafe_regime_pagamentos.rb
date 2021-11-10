092ab3715127d3d38858b13993651f13dea8defd
jeovan farias <jeovan.farias@sefin.ro.gov.br>
2021-11-10 18:01:56 -0400

    atualizaçao

--- db/migrate/20211109233229_create_sitafe_sitafe_regime_pagamentos.rb
+++
@@ -1,8 +0,0 @@
-class CreateSitafeSitafeRegimePagamentos < ActiveRecord::Migration[6.1]
-  def change
-    create_table :sitafe_sitafe_regime_pagamentos do |t|
-
-      t.timestamps
-    end
-  end
-end
