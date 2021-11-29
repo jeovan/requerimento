b104455fbacda181582ac298b3493a7447de8d7e
jeovan farias <jeovan.farias@sefin.ro.gov.br>
2021-11-29 06:16:22 -0400

    att

--- app/models/requerimento.rb
+++ app/models/requerimento.rb
@@ -12,6 +12,53 @@ class Requerimento
 
 
   validates :tipo_servico, presence:{ message: ' : precisa ser selecionado'}
+  validates :cnpj, length: {is: 14}, presence:{ message: ' : precisa ser selecionado'}
+  validates :razao_social, presence:{ message: ' : precisa ser selecionado'}
+  validates :nome_fantasia, presence:{ message: ' : precisa ser selecionado'}
+  validates :utilizacao_estab, presence:{ message: ' : precisa ser selecionado'}
+
+  validates :logradouro_comercial, presence:{ message: ' : precisa ser selecionado'}
+  # validates :complemento, presence:{ message: ' : precisa ser selecionado'}
+  validates :numero, presence:{ message: ' : precisa ser selecionado'}
+  validates :bairro_comercial, presence:{ message: ' : precisa ser selecionado'}
+  validates :municipio_comercial, presence:{ message: ' : precisa ser selecionado'}
+  validates :cep, presence:{ message: ' : precisa ser selecionado'}
+  validates :alvara, presence:{ message: ' : precisa ser selecionado'}
+  validates :area_utilizada, presence:{ message: ' : precisa ser selecionado'}
+
+  validates :logradouro, presence:{ message: ' : precisa ser selecionado'}, if: ->{ self.copia_endereco_comercial.blank?}
+  validates :bairro, presence:{ message: ' : precisa ser selecionado'}
+  validates :municipio_corresp, presence:{ message: ' : precisa ser selecionado'}
+  validates :cep, presence:{ message: ' : precisa ser selecionado'}
+  validates :telefone, presence:{ message: ' : precisa ser selecionado'}
+  validates :email, presence:{ message: ' : precisa ser selecionado'}
+
+
+  validates :natureza_juridica, presence:{ message: ' : precisa ser selecionado'}
+  validates :regime_pagamento, presence:{ message: ' : precisa ser selecionado'}
+  validates :atividade_economica, presence:{ message: ' : precisa ser selecionado'}
+
+
+  validates :contador, presence:{ message: ' : precisa ser selecionado'}
+  validates :crc, presence:{ message: ' : precisa ser selecionado'}
+  validates :nome_contador, presence:{ message: ' : precisa ser selecionado'}
+  validates :bairro, presence:{ message: ' : precisa ser selecionado'}
+  # validates :distrito, presence:{ message: ' : precisa ser selecionado'}
+  validates :municipio_contabilista, presence:{ message: ' : precisa ser selecionado'}
+
+  validates :telefone_1_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :telefone_2_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :email_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :cep_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :nome_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :id_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :cargo_socio, presence:{ message: ' : precisa ser selecionado'}
+  validates :municipio_socio, presence:{ message: ' : precisa ser selecionado'}
+
+
+
+
+
 
 
 end
