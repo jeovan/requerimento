4654275e07aee9e38909fa5f22488c66fde1011e
Jeovan da Silva Farias <02639209256@sefin.ro.gov.br>
2021-11-29 12:54:05 -0400

    att

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,11 @@
 //= require rails-ujs
 //= require_tree .
 
-
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
 
 
 
@@ -18,12 +22,15 @@ $(document).ready(function(){
             keys.push(inputs[key].id)
             socio[inputs[key].name] = inputs[key].value
         })
+        
+        var id = $('.linha-socio').length + 1
 
         var linha = `
         <tr>
             <td>${socio['requerimento[id_socio]']}</td>
-            <td class="text-center">
-            <input type="hidden" name="lista_socios[]" value="${socio}"/>
+            <td class="text-center linha-socio">
+                
+                <input type="hidden" name="requerimento[lista_socios[${uuidv4()}]]" value="${window.btoa(unescape(encodeURIComponent( JSON.stringify(socio) )))}"/>
                 <button type="button" style="padding:10px 10px;" class="btn btn-danger btn-raised legitRipple btn-remove-socio">
                 <i class=" icon-trash"></i>
                 </button>
--- app/controllers/requerimentos_controller.rb
+++ app/controllers/requerimentos_controller.rb
@@ -1,3 +1,5 @@
+ require 'base64'
+
 class RequerimentosController < ApplicationController
   def index
     @requerimento = Requerimento.new
@@ -5,6 +7,12 @@ class RequerimentosController < ApplicationController
 
   def create
     @requerimento = Requerimento.new(params_formulario)
+    # puts '==============================='
+    # puts params[:requerimento_lista_socios].inspect
+    # puts '-------------------------------'
+    # puts @requerimento.lista_socios
+    # puts Base64.decode64(@requerimento.lista_socios["0"])
+    # puts '----------------------'
     if @requerimento.valid?
       #----
     else
